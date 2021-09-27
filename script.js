8eaa77f2e7e9284d977fc00b674ab07884f59869
Collins Ndegwa <collinshomen@gmail.com>
2021-09-27 11:15:41 +0200

    transfer laptop

---
+++ script.js
@@ -0,0 +1,11 @@
+
+document.getElementById("melafricabtn").addEventListener("click",melafricalink);
+document.getElementById("tnabtn").addEventListener("click",tnalink);
+
+function melafricalink(){
+   window.open('https://melafricanation.netlify.app', '_blank');
+}
+
+function tnalink(){
+   window.open('https://tnaproperties.netlify.app', '_blank');
+}
