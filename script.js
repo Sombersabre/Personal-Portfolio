f1e32a926f38d14e610c82dd85552de68d90698f
Collins Ndegwa <collinshomen@gmail.com>
2021-10-01 17:57:59 +0200

    update layout

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ function melafricalink(){
 function tnalink(){
    window.open('https://tnaproperties.netlify.app', '_blank');
 }
+
+function maillink(){
+   let subject = "Communication from website portfolio";
+   let body = "Hi, My name is (name) and I'd like to know more about what you do. I'm available at (phone number) or (email)";
+   window.open('mailto:collinsnbusiness@gmail.com?subject='+subject+'&body='+body);
+}
